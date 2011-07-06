6227f52befc0f6dd16cc5e4a12a98283f9703da4
Adam Hernandez <adam@digimedia.com>
2011-07-06 14:39:07 -0500

    currency gem version of problematic app

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,11 +1,5 @@
 class Invoice < ActiveRecord::Base
-  attr_accessor_with_default :currency, "USD"
-  
-  composed_of :total_price,
-    :class_name => "Money",
-    :mapping => [%w(total_price cents), %w(currency currency_as_string)],
-    :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
-    :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
+  attr_money :total_price
   
   has_many :invoice_items, :dependent => :destroy
   
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Invoices::Application.initialize!
+
+require "currency"
+
+class ActiveRecord::Base
+  require "currency/active_record"
+end
